e38af6c05d06352a6e1b179bb8c592a4a77a73e7
seok-min-seo <51194291+seok-min-seo@users.noreply.github.com>
2019-07-07 01:04:14 +0900

    3-2

---
+++ Unity-2D-Rhythm-Game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //싱글톤 형태로 처리
+
+        public static GameManager instance { get; set; }
+    private void Awake()
+    {
+        if (instance == null) instance = this;
+        else if (instance != this) Destroy(gameObject);
+    }
+
+    public float notespeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Unity-2D-Rhythm-Game/Assets/Scripts/NoteBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteBehaviour : MonoBehaviour
+{
+    public int noteType;
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.down * GameManager.instance.notespeed);        
+    }
+}
