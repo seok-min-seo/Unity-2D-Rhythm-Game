e1332bb4b206345ff4cb531a505695a9547e4768
seok-min-seo <51194291+seok-min-seo@users.noreply.github.com>
2019-07-10 02:35:27 +0900

    dd

--- Unity-2D-Rhythm-Game/Assets/Scripts/GameResultManager.cs
+++ Unity-2D-Rhythm-Game/Assets/Scripts/GameResultManager.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.IO;
+using System;
 
 public class GameResultManager : MonoBehaviour
 {
@@ -9,19 +12,55 @@ public class GameResultManager : MonoBehaviour
     public Text musicTitleUI;
     public Text scoreUI;
     public Text maxComboUI;
+    public Image RankUI;
 
     // Start is called before the first frame update
     void Start()
     {
         musicTitleUI.text = PlayerInformation.musicTitle;
-        scoreUI.text = "" + PlayerInformation.score;
+        scoreUI.text = "점수: " + (int)PlayerInformation.score;
         //숫자형태를 문자열로 바꾸기 위해선 앞에 ""를 더해주면된다
-        maxComboUI.text = "" + PlayerInformation.maxCombo;
+        maxComboUI.text = "최대 콤보: " + PlayerInformation.maxCombo;
+        //리소스에서 비트(Beat) 텍스트 파일을 불러옵니다.
+        TextAsset textAsset = Resources.Load<TextAsset>("Beats/" + PlayerInformation.selectedMusic);
+        StringReader reader = new StringReader(textAsset.text);
+        //첫번쨰 줄과 두번쨰 줄을 무시합니다.
+        reader.ReadLine();
+        reader.ReadLine();
+        // 세번쨰 줄에 적힌 비트 정보(S랭크 점수 , A랭크 점수 , B랭크 점수)를 읽습니다.
+        string beatInformation = reader.ReadLine();
+        int scoreS = Convert.ToInt32(beatInformation.Split(' ')[3]);
+        int scoreA = Convert.ToInt32(beatInformation.Split(' ')[4]);
+        int scoreB = Convert.ToInt32(beatInformation.Split(' ')[5]);
+        //성적에 맞는 랭크 이미지를 불러옵니다.
+        if(PlayerInformation.score >= scoreS)
+        {
+            RankUI.sprite = Resources.Load<Sprite>("Sprites/Rank S");
 
-        Debug.Log(PlayerInformation.maxCombo);
+        }
+        else if (PlayerInformation.score >= scoreA)
+        {
+            RankUI.sprite = Resources.Load<Sprite>("Sprites/Rank A");
+
+        }
+        else if (PlayerInformation.score >= scoreB)
+        {
+            RankUI.sprite = Resources.Load<Sprite>("Sprites/Rank B");
+
+        }
+        else
+        {
+            RankUI.sprite = Resources.Load<Sprite>("Sprites/Rank C");
+        }
+
 
     }
 
+    public void Replay()
+    {
+        SceneManager.LoadScene("SongSelectScene");
+    }
+    
     // Update is called once per frame
     void Update()
     {
---
+++ Unity-2D-Rhythm-Game/Assets/Scripts/SongSelectManager.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+using UnityEngine.SceneManagement;
+
+public class SongSelectManager : MonoBehaviour
+{
+    public Image musicImageUI;
+    public Text musicTitleUI;
+    public Text bpmUI;
+
+    private int musicIndex;
+    private int musicCount = 4;
+
+    private void UpdateSong(int musicIndex)
+    {
+        AudioSource audioSource = GetComponent<AudioSource>();
+        audioSource.Stop();
+        //리소스에서 비트(Beat) 텍스트 파일을 불러옵니다.
+        TextAsset textAsset = Resources.Load<TextAsset>("Beats/" + musicIndex.ToString());
+        StringReader stringReader = new StringReader(textAsset.text);
+        //첫번째 줄에 적힌 곡 이름을 읽어서 UI를 업데이트 합니다
+        musicTitleUI.text = stringReader.ReadLine();
+        //두번째 줄은 읽기만 하고 아무 처리도 하지않습니다.
+        stringReader.ReadLine();
+        //세번째 줄에 적힌 BPM을 읽어서 UI를 업데이트합니다
+        bpmUI.text = stringReader.ReadLine().Split(' ')[0];
+        //리소스에서 비트 (Beat) 음악 파일을 불러와 재생합니다.
+        AudioClip audioClip = Resources.Load<AudioClip>("Beats/" + musicIndex.ToString());
+        audioSource.clip = audioClip;
+        audioSource.Play();
+        //리소스에서 비트(Beat)이미지 파일을 불러옵니다.
+        musicImageUI.sprite = Resources.Load<Sprite>("Beats/" + musicIndex.ToString());
+
+    }
+
+    public void Right()
+    {
+        musicIndex = musicIndex + 1;
+        if (musicIndex > musicCount) musicIndex = 1;
+        UpdateSong(musicIndex);
+    }
+
+    public void Left()
+    {
+        musicIndex = musicIndex - 1;
+        if (musicIndex < 1) musicIndex = musicCount;
+        UpdateSong(musicIndex);
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        musicIndex = 1;
+        UpdateSong(musicIndex);
+    }
+
+    public void GameStart()
+    {
+        PlayerInformation.selectedMusic = musicIndex.ToString();
+        SceneManager.LoadScene("GameScene");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
