9d29502529ec38c15bde58b7d6b8004805e9a8ce
seok-min-seo <51194291+seok-min-seo@users.noreply.github.com>
2019-07-09 18:30:38 +0900

    dd

--- Unity-2D-Rhythm-Game/Assets/Scripts/NoteController.cs
+++ Unity-2D-Rhythm-Game/Assets/Scripts/NoteController.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class NoteController : MonoBehaviour
@@ -22,8 +24,7 @@ public class NoteController : MonoBehaviour
     private ObjectPooler noteObjectPooler;
     private List<Note> notes = new List<Note>();
     private float x, z, startY = 8.0f;
-    private float beatInterval = 1.0f;
-
+   
     void MakeNote(Note note)
     {
         GameObject obj = noteObjectPooler.getObject(note.noteType);
@@ -35,11 +36,20 @@ public class NoteController : MonoBehaviour
         obj.SetActive(true);
     }
 
+    private string musicTitle;
+    private string musicArtist;
+    private int bpm;
+    private int divider;
+    private float startingPoint;
+    private float beatCount;
+    private float beatInterval;
+
+
     IEnumerator AwaitMakeNote(Note note)
     {
         int noteType = note.noteType;
         int order = note.order;
-        yield return new WaitForSeconds(order * beatInterval);
+        yield return new WaitForSeconds(startingPoint + order * beatInterval);
         MakeNote(note);
     }
 
@@ -47,14 +57,39 @@ public class NoteController : MonoBehaviour
     void Start()
     {
         noteObjectPooler = gameObject.GetComponent<ObjectPooler>();
-        notes.Add(new Note(1, 1));
-        notes.Add(new Note(2, 2));
-        notes.Add(new Note(3, 3));
-        notes.Add(new Note(4, 4));
-        notes.Add(new Note(1, 5));
-        notes.Add(new Note(2, 6));
-        notes.Add(new Note(3, 7));
-        notes.Add(new Note(4, 8));
+        // 리소스에서 비트(Beat) 텍스트 파일을 불러옵니다.
+        TextAsset textAsset = Resources.Load<TextAsset>("Beats/" + GameManager.instance.music);
+        StringReader reader = new StringReader(textAsset.text);
+        //첫번쨰 줄에 적힌 곡 이름을 읽습니다.
+        musicTitle = reader.ReadLine();
+        //두번쨰 줄에 적힌 아티스트 이름을 읽습니다.
+        musicArtist = reader.ReadLine();
+        //세번째 줄에 적힌 비트 정보(BPM, Divider, 시작 시간)를 읽습니다.
+        string beatInformation = reader.ReadLine();
+        bpm = Convert.ToInt32(beatInformation.Split(' ')[0]);
+        divider = Convert.ToInt32(beatInformation.Split(' ')[1]);
+        startingPoint = (float) Convert.ToDouble(beatInformation.Split(' ')[2]);
+        //1초마다 떨어지는 비트 개수
+        beatCount = (float)bpm / divider;
+        // beatCount : 160 / 30 => 16 / 3 
+        //비트가 떨어지는 간격 시간
+        beatInterval = 1 / beatCount;
+        //각 비트들이 떨어지는 위치 및 시간 정보를 읽습니다
+        string line;
+        while((line = reader.ReadLine()) != null)
+        {
+            Note note = new Note(
+                Convert.ToInt32(line.Split(' ')[0]) +1,  
+                //공백을 기준으로 나눠서 첫번쨰는 +1된 노트자리를 반환하고(노트자리는 4개)
+                Convert.ToInt32(line.Split(' ')[1]) 
+                //뒤에는 몇초에 시작할지를 반환
+                );
+            notes.Add(note); //미리 만들어둔 노트 리스트에 모두 담겨서 떨어지게됨
+        }
+
+        //beatInformation : "160 30 3.5"
+
+
         //  모든 노트를 정해진 시간에 출발하도록 설정
         for (int i = 0; i < notes.Count; i++)
         {
